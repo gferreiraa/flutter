c8071e20fa85f1bff1c7ca418b07f7b1099a0ec6
gferreiraa <gferreiraa@gmail.com>
2019-06-15 14:51:30 -0300

    Cara ou Coroa - Concluído

--- caraoucoroa/lib/TelaPrincipal.dart
+++ caraoucoroa/lib/TelaPrincipal.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import 'Resultado.dart';
 
 class Jogar extends StatefulWidget {
   @override
@@ -9,6 +13,16 @@ class _JogarState extends State<Jogar> {
 
   void _exibirResultado(){
 
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Resultado(resultado)
+        )
+    );
   }
 
   @override
